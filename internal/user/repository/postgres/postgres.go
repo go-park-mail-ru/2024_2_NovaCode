f9587b5aead341a441623ddda230e8910caa2cef
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-26 09:12:04 +0300

    implement postgres repository

---
+++ internal/user/repository/postgres/postgres.go
@@ -0,0 +1,118 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/daronenko/auth/internal/models"
+	"github.com/daronenko/auth/internal/user"
+	"github.com/google/uuid"
+)
+
+const (
+	insertUserQuery = `
+		INSERT INTO "user" (username, email, password, role)
+		VALUES ($1, $2, $3, $4)
+		RETURNING user_id, username, email, password, role, created_at, updated_at
+	`
+	findByIDQuery = `
+		SELECT user_id, username, email, role, password, created_at, updated_at
+		FROM "user" WHERE user_id = $1
+	`
+	findByUsernameQuery = `
+		SELECT user_id, username, email, role, password, created_at, updated_at
+		FROM "user" WHERE username = $1
+	`
+	findByEmailQuery = `
+		SELECT user_id, username, email, role, password, created_at, updated_at
+		FROM "user" WHERE email = $1
+	`
+)
+
+type UserPostgresRepo struct {
+	db *sql.DB
+}
+
+func NewUserPostgresRepository(db *sql.DB) user.Repo {
+	return &UserPostgresRepo{db}
+}
+
+func (repo *UserPostgresRepo) Insert(ctx context.Context, user *models.User) (*models.User, error) {
+	var insertedUser models.User
+
+	if err := repo.db.QueryRowContext(
+		ctx,
+		insertUserQuery,
+		user.Username,
+		user.Email,
+		user.Password,
+		user.Role,
+	).Scan(
+		&insertedUser.UserID,
+		&insertedUser.Username,
+		&insertedUser.Email,
+		&insertedUser.Password,
+		&insertedUser.Role,
+		&insertedUser.CreatedAt,
+		&insertedUser.UpdatedAt,
+	); err != nil {
+		return nil, fmt.Errorf("failed to insert user: %w", err)
+	}
+
+	return &insertedUser, nil
+}
+
+func (repo *UserPostgresRepo) FindByID(ctx context.Context, uuid uuid.UUID) (*models.User, error) {
+	var user models.User
+
+	if err := repo.db.QueryRowContext(ctx, findByIDQuery, uuid).Scan(
+		&user.UserID,
+		&user.Username,
+		&user.Email,
+		&user.Role,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	); err != nil {
+		return nil, fmt.Errorf("failed to find user by ID: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (repo *UserPostgresRepo) FindByUsername(ctx context.Context, username string) (*models.User, error) {
+	var user models.User
+
+	if err := repo.db.QueryRowContext(ctx, findByUsernameQuery, username).Scan(
+		&user.UserID,
+		&user.Username,
+		&user.Email,
+		&user.Role,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	); err != nil {
+		return nil, fmt.Errorf("failed to find user by username: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (repo *UserPostgresRepo) FindByEmail(ctx context.Context, email string) (*models.User, error) {
+	var user models.User
+
+	if err := repo.db.QueryRowContext(ctx, findByEmailQuery, email).Scan(
+		&user.UserID,
+		&user.Username,
+		&user.Email,
+		&user.Role,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	); err != nil {
+		return nil, fmt.Errorf("failed to find user by email: %w", err)
+	}
+
+	return &user, nil
+}
---
+++ internal/user/repository/postgres/postgres_test.go
@@ -0,0 +1,305 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/daronenko/auth/internal/models"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInsert_Regular(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	columns := []string{"user_id", "username", "email", "password", "role", "created_at", "updated_at"}
+
+	userUUID := uuid.New()
+	userMock := &models.User{
+		UserID:   userUUID,
+		Username: "test_user",
+		Email:    "test@email.com",
+		Password: "password",
+		Role:     "regular",
+	}
+
+	rows := sqlmock.NewRows(columns).AddRow(
+		userUUID,
+		userMock.Username,
+		userMock.Email,
+		userMock.Password,
+		userMock.Role,
+		time.Now(),
+		time.Now(),
+	)
+
+	mock.ExpectQuery(insertUserQuery).WithArgs(
+		userMock.Username,
+		userMock.Email,
+		userMock.Password,
+		userMock.Role,
+	).WillReturnRows(rows)
+
+	insertedUser, err := postgresRepo.Insert(context.Background(), userMock)
+	require.NotNil(t, insertedUser)
+	require.NoError(t, err)
+}
+
+func TestInsert_Error(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	userMock := &models.User{
+		Username: "test_user",
+		Email:    "test@email.com",
+		Password: "password",
+		Role:     "regular",
+	}
+
+	mock.ExpectQuery(insertUserQuery).WithArgs(
+		userMock.Username,
+		userMock.Email,
+		userMock.Password,
+		userMock.Role,
+	).WillReturnError(fmt.Errorf("some error"))
+
+	_, err = postgresRepo.Insert(context.Background(), userMock)
+	require.Contains(t, err.Error(), "failed to insert user")
+	require.Error(t, err)
+}
+
+func TestFindByID_Regular(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	columns := []string{"user_id", "username", "email", "password", "role", "created_at", "updated_at"}
+
+	userUUID := uuid.New()
+	userMock := &models.User{
+		UserID:   userUUID,
+		Username: "test_user",
+		Email:    "test@email.com",
+		Password: "password",
+		Role:     "admin",
+	}
+
+	rows := sqlmock.NewRows(columns).AddRow(
+		userUUID,
+		userMock.Username,
+		userMock.Email,
+		userMock.Password,
+		userMock.Role,
+		time.Now(),
+		time.Now(),
+	)
+
+	mock.ExpectQuery(findByIDQuery).WithArgs(userMock.UserID).WillReturnRows(rows)
+
+	foundUser, err := postgresRepo.FindByID(context.Background(), userMock.UserID)
+	require.NoError(t, err)
+	require.NotNil(t, foundUser)
+	require.Equal(t, foundUser.UserID, userMock.UserID)
+}
+
+func TestFindByID_Error(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	userUUID := uuid.New()
+
+	mock.ExpectQuery(findByIDQuery).WithArgs(userUUID).WillReturnError(fmt.Errorf("some error"))
+
+	_, err = postgresRepo.FindByID(context.Background(), userUUID)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find user by ID")
+}
+
+func TestFindByID_NotFound(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	userUUID := uuid.New()
+
+	mock.ExpectQuery(findByIDQuery).WithArgs(userUUID).WillReturnRows(sqlmock.NewRows(nil))
+
+	_, err = postgresRepo.FindByID(context.Background(), userUUID)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find user by ID")
+}
+
+func TestFindByUsername_Regular(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	columns := []string{"user_id", "username", "email", "password", "role", "created_at", "updated_at"}
+
+	userUUID := uuid.New()
+	userMock := &models.User{
+		UserID:   userUUID,
+		Username: "test_user",
+		Email:    "test@email.com",
+		Password: "password",
+		Role:     "regular",
+	}
+
+	rows := sqlmock.NewRows(columns).AddRow(
+		userUUID,
+		userMock.Username,
+		userMock.Email,
+		userMock.Password,
+		userMock.Role,
+		time.Now(),
+		time.Now(),
+	)
+
+	mock.ExpectQuery(findByUsernameQuery).WithArgs(userMock.Username).WillReturnRows(rows)
+
+	foundUser, err := postgresRepo.FindByUsername(context.Background(), userMock.Username)
+	require.NoError(t, err)
+	require.NotNil(t, foundUser)
+	require.Equal(t, foundUser.UserID, userMock.UserID)
+}
+
+func TestFindByUsername_Error(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	username := "test_user"
+
+	mock.ExpectQuery(findByUsernameQuery).WithArgs(username).WillReturnError(fmt.Errorf("some error"))
+
+	_, err = postgresRepo.FindByUsername(context.Background(), username)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find user by username")
+}
+
+func TestFindByUsername_NotFound(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	username := "test_user"
+
+	mock.ExpectQuery(findByUsernameQuery).WithArgs(username).WillReturnRows(sqlmock.NewRows(nil))
+
+	_, err = postgresRepo.FindByUsername(context.Background(), username)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find user by username")
+}
+
+func TestFindByEmail_Regular(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	columns := []string{"user_id", "username", "email", "password", "role", "created_at", "updated_at"}
+
+	userUUID := uuid.New()
+	userMock := &models.User{
+		UserID:   userUUID,
+		Username: "test_user",
+		Email:    "test@email.com",
+		Password: "password",
+		Role:     "admin",
+	}
+
+	rows := sqlmock.NewRows(columns).AddRow(
+		userUUID,
+		userMock.Username,
+		userMock.Email,
+		userMock.Password,
+		userMock.Role,
+		time.Now(),
+		time.Now(),
+	)
+
+	mock.ExpectQuery(findByEmailQuery).WithArgs(userMock.Email).WillReturnRows(rows)
+
+	foundUser, err := postgresRepo.FindByEmail(context.Background(), userMock.Email)
+	require.NoError(t, err)
+	require.NotNil(t, foundUser)
+	require.Equal(t, foundUser.UserID, userMock.UserID)
+}
+
+func TestFindByEmail_Error(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	email := "test@email.com"
+
+	mock.ExpectQuery(findByEmailQuery).WithArgs(email).WillReturnError(fmt.Errorf("some error"))
+
+	_, err = postgresRepo.FindByEmail(context.Background(), email)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find user by email")
+}
+
+func TestFindByEmail_NotFound(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	postgresRepo := NewUserPostgresRepository(db)
+
+	email := "test@email.com"
+
+	mock.ExpectQuery(findByEmailQuery).WithArgs(email).WillReturnRows(sqlmock.NewRows(nil))
+
+	_, err = postgresRepo.FindByEmail(context.Background(), email)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find user by email")
+}
