7bea7779935b17c3a94a7c74b6b700001d43157b
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-26 08:55:13 +0300

    set up logging

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,37 @@
+package postgres
+
+import (
+	"fmt"
+	"time"
+
+	"database/sql"
+
+	"github.com/daronenko/auth/config"
+	_ "github.com/lib/pq"
+)
+
+func New(cfg *config.Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		cfg.Postgres.Host,
+		cfg.Postgres.Port,
+		cfg.Postgres.User,
+		cfg.Postgres.DBName,
+		cfg.Postgres.Password,
+	)
+
+	db, err := sql.Open(cfg.Postgres.Driver, connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.Postgres.MaxOpenConns)
+	db.SetConnMaxLifetime(time.Duration(cfg.Postgres.ConnMaxIdleLifetime) * time.Second)
+	db.SetMaxIdleConns(cfg.Postgres.MaxIdleConns)
+	db.SetConnMaxIdleTime(time.Duration(cfg.Postgres.ConnMaxIdleTime) * time.Second)
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("cannot ping postgres instance: %v", err)
+	}
+
+	return db, nil
+}
