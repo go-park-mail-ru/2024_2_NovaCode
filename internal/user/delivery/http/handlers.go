ee327aa3f8bc2b529bd02002e748491e5c62927c
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-26 09:15:23 +0300

    implement delivery layer

---
+++ internal/user/delivery/http/handlers.go
@@ -0,0 +1,172 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/daronenko/auth/config"
+	"github.com/daronenko/auth/internal/models"
+	"github.com/daronenko/auth/internal/user"
+	"github.com/daronenko/auth/internal/utils"
+	"github.com/daronenko/auth/pkg/logger"
+)
+
+type userHandlers struct {
+	cfg     *config.Config
+	usecase user.Usecase
+	logger  logger.Logger
+}
+
+func NewUserHandlers(cfg *config.Config, usecase user.Usecase, logger logger.Logger) user.Handlers {
+	return &userHandlers{cfg, usecase, logger}
+}
+
+// Health godoc
+// @Summary Health check
+// @Description Returns "OK" if the service is running
+// @Success 200 {string} utils.MessageResponse "OK"
+// @Router /api/v1/health [get]
+func (handlers *userHandlers) Health(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(response).Encode(utils.NewMessageResponse("OK")); err != nil {
+		handlers.logger.Errorf("error encoding health response: %v", err)
+		utils.JSONError(response, http.StatusInternalServerError, "failed to get health status")
+		return
+	}
+}
+
+// Register godoc
+// @Summary Register new user
+// @Description Register new user with username, email and password
+// @Accept json
+// @Produce json
+// @Param user body models.User true "User registration details"
+// @Success 200 {object} dto.UserTokenDTO "User registration successful"
+// @Failure 400 {object} utils.ErrorResponse "Invalid request body or missing fields"
+// @Failure 500 {object} utils.ErrorResponse "Failed to return token"
+// @Router /api/v1/auth/register [post]
+func (handlers *userHandlers) Register(response http.ResponseWriter, request *http.Request) {
+	var user models.User
+
+	if err := json.NewDecoder(request.Body).Decode(&user); err != nil {
+		utils.JSONError(response, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	if user.Username == "" || user.Email == "" || user.Password == "" {
+		utils.JSONError(response, http.StatusBadRequest, "username, email and password are required")
+		return
+	}
+
+	if user.Role == "" {
+		user.Role = "regular"
+	}
+
+	userTokenDTO, err := handlers.usecase.Register(request.Context(), &user)
+	if err != nil {
+		handlers.logger.Errorf("failed to register user: %v", err)
+		utils.JSONError(response, http.StatusBadRequest, "invalid username or password")
+		return
+	}
+
+	accessTokenCookie := http.Cookie{
+		Name:     handlers.cfg.Auth.Jwt.Cookie.Name,
+		Value:    userTokenDTO.Token,
+		MaxAge:   handlers.cfg.Auth.Jwt.Cookie.MaxAge,
+		Secure:   handlers.cfg.Auth.Jwt.Cookie.Secure,
+		HttpOnly: handlers.cfg.Auth.Jwt.Cookie.HttpOnly,
+	}
+
+	http.SetCookie(response, &accessTokenCookie)
+
+	response.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(response).Encode(userTokenDTO); err != nil {
+		handlers.logger.Errorf("error encoding userTokenDTO: %v", err)
+		utils.JSONError(response, http.StatusInternalServerError, "failed to return token")
+		return
+	}
+
+	handlers.logger.Infof("userTokenDTO: %v", userTokenDTO)
+	handlers.logger.Infof("userTokenDTO.user: %v", userTokenDTO.User)
+
+	response.WriteHeader(http.StatusOK)
+}
+
+// Login godoc
+// @Summary Login user
+// @Description Authenticate user with username and password
+// @Accept json
+// @Produce json
+// @Param user body models.User true "User login details"
+// @Success 200 {object} dto.UserTokenDTO "Login successful"
+// @Failure 400 {object} utils.ErrorResponse "Invalid request body or missing fields"
+// @Failure 401 {object} utils.ErrorResponse "Invalid username or password"
+// @Failure 500 {object} utils.ErrorResponse "Failed to return token"
+// @Router /api/v1/auth/login [post]
+func (handlers *userHandlers) Login(response http.ResponseWriter, request *http.Request) {
+	var user models.User
+
+	if err := json.NewDecoder(request.Body).Decode(&user); err != nil {
+		utils.JSONError(response, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	if user.Username == "" || user.Password == "" {
+		utils.JSONError(response, http.StatusBadRequest, "username and password are required")
+		return
+	}
+
+	userTokenDTO, err := handlers.usecase.Login(request.Context(), &user)
+	if err != nil {
+		handlers.logger.Warnf("failed to login user: %v", err)
+		utils.JSONError(response, http.StatusUnauthorized, "invalid username or password")
+		return
+	}
+
+	accessTokenCookie := http.Cookie{
+		Name:     handlers.cfg.Auth.Jwt.Cookie.Name,
+		Value:    userTokenDTO.Token,
+		MaxAge:   handlers.cfg.Auth.Jwt.Cookie.MaxAge,
+		Secure:   handlers.cfg.Auth.Jwt.Cookie.Secure,
+		HttpOnly: handlers.cfg.Auth.Jwt.Cookie.HttpOnly,
+	}
+
+	http.SetCookie(response, &accessTokenCookie)
+
+	response.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(response).Encode(userTokenDTO); err != nil {
+		handlers.logger.Errorf("failed to encode userTokenDTO: %v", err)
+		utils.JSONError(response, http.StatusInternalServerError, "failed to return token")
+		return
+	}
+
+	response.WriteHeader(http.StatusOK)
+}
+
+// Logout godoc
+// @Summary Log out user
+// @Description Log out user and clear access token cookie
+// @Success 200 {object} utils.MessageResponse "Logout successful"
+// @Failure 500 {object} utils.ErrorResponse "Failed to log out"
+// @Router /api/v1/auth/logout [post]
+func (handlers *userHandlers) Logout(response http.ResponseWriter, request *http.Request) {
+	accessTokenCookie := http.Cookie{
+		Name:     handlers.cfg.Auth.Jwt.Cookie.Name,
+		Value:    "",
+		MaxAge:   -1,
+		Secure:   handlers.cfg.Auth.Jwt.Cookie.Secure,
+		HttpOnly: handlers.cfg.Auth.Jwt.Cookie.HttpOnly,
+	}
+
+	http.SetCookie(response, &accessTokenCookie)
+
+	response.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(response).Encode(utils.NewMessageResponse("successfully logged out")); err != nil {
+		handlers.logger.Errorf("error encoding logout response: %v", err)
+		utils.JSONError(response, http.StatusInternalServerError, "failed to log out")
+		return
+	}
+
+	response.WriteHeader(http.StatusOK)
+}
---
+++ internal/user/delivery/http/handlers_test.go
@@ -0,0 +1,253 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/daronenko/auth/config"
+	"github.com/daronenko/auth/internal/models"
+	"github.com/daronenko/auth/internal/user/dto"
+	"github.com/daronenko/auth/internal/user/mock"
+	"github.com/daronenko/auth/pkg/logger"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserHandlers_Register(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cfg := &config.Config{
+		Auth: config.AuthConfig{
+			Jwt: config.JwtConfig{
+				Cookie: config.JwtCookieConfig{
+					Name:     "access_token",
+					MaxAge:   3600,
+					Secure:   true,
+					HttpOnly: true,
+				},
+			},
+		},
+	}
+
+	logger := logger.New(cfg)
+	usecaseMock := mock.NewMockUsecase(ctrl)
+	userHandlers := NewUserHandlers(cfg, usecaseMock, logger)
+
+	t.Run("successful registration", func(t *testing.T) {
+		user := models.User{
+			Username: "test_user",
+			Email:    "test@example.com",
+			Password: "password",
+			Role:     "regular",
+		}
+
+		userTokenDTO := &dto.UserTokenDTO{
+			Token: "test_token",
+		}
+
+		usecaseMock.EXPECT().Register(gomock.Any(), &user).Return(userTokenDTO, nil)
+
+		body, _ := json.Marshal(user)
+		request := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		userHandlers.Register(response, request)
+
+		res := response.Result()
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+
+		cookie := response.Result().Cookies()[0]
+		assert.Equal(t, cfg.Auth.Jwt.Cookie.Name, cookie.Name)
+		assert.Equal(t, userTokenDTO.Token, cookie.Value)
+		assert.Equal(t, cfg.Auth.Jwt.Cookie.MaxAge, cookie.MaxAge)
+		assert.True(t, cookie.HttpOnly)
+		assert.True(t, cookie.Secure)
+	})
+
+	t.Run("invalid request body", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBufferString("invalid json"))
+		response := httptest.NewRecorder()
+
+		userHandlers.Register(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode)
+	})
+
+	t.Run("missing required fields", func(t *testing.T) {
+		user := models.User{
+			Username: "",
+			Email:    "test@example.com",
+			Password: "password",
+		}
+
+		body, _ := json.Marshal(user)
+		request := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		userHandlers.Register(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode)
+	})
+
+	t.Run("usecase registration error", func(t *testing.T) {
+		user := models.User{
+			Username: "test_user",
+			Email:    "test@example.com",
+			Password: "password",
+			Role:     "regular",
+		}
+
+		usecaseMock.EXPECT().Register(gomock.Any(), &user).Return(nil, errors.New("registration error"))
+
+		body, _ := json.Marshal(user)
+		request := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		userHandlers.Register(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode)
+	})
+}
+
+func TestUserHandlers_Login(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cfg := &config.Config{
+		Auth: config.AuthConfig{
+			Jwt: config.JwtConfig{
+				Cookie: config.JwtCookieConfig{
+					Name:     "access_token",
+					MaxAge:   3600,
+					Secure:   true,
+					HttpOnly: true,
+				},
+			},
+		},
+	}
+
+	logger := logger.New(cfg)
+	usecaseMock := mock.NewMockUsecase(ctrl)
+	userHandlers := NewUserHandlers(cfg, usecaseMock, logger)
+
+	t.Run("successful login", func(t *testing.T) {
+		user := models.User{
+			Username: "test_user",
+			Password: "password",
+		}
+
+		userTokenDTO := &dto.UserTokenDTO{
+			Token: "test_token",
+		}
+
+		usecaseMock.EXPECT().Login(gomock.Any(), &user).Return(userTokenDTO, nil)
+
+		body, _ := json.Marshal(user)
+		request := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		userHandlers.Login(response, request)
+
+		result := response.Result()
+		assert.Equal(t, http.StatusOK, result.StatusCode)
+
+		cookie := response.Result().Cookies()[0]
+		assert.Equal(t, cfg.Auth.Jwt.Cookie.Name, cookie.Name)
+		assert.Equal(t, userTokenDTO.Token, cookie.Value)
+		assert.Equal(t, cfg.Auth.Jwt.Cookie.MaxAge, cookie.MaxAge)
+		assert.True(t, cookie.HttpOnly)
+		assert.True(t, cookie.Secure)
+
+		var responseDTO dto.UserTokenDTO
+		err := json.NewDecoder(response.Body).Decode(&responseDTO)
+		assert.NoError(t, err)
+		assert.Equal(t, userTokenDTO.Token, responseDTO.Token)
+	})
+
+	t.Run("invalid request body", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBufferString("invalid json"))
+		response := httptest.NewRecorder()
+
+		userHandlers.Login(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode)
+	})
+
+	t.Run("missing required fields", func(t *testing.T) {
+		user := models.User{
+			Username: "",
+			Password: "",
+		}
+
+		body, _ := json.Marshal(user)
+		request := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		userHandlers.Login(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode)
+	})
+
+	t.Run("usecase login error", func(t *testing.T) {
+		user := models.User{
+			Username: "test_user",
+			Password: "password",
+		}
+
+		usecaseMock.EXPECT().Login(gomock.Any(), &user).Return(nil, errors.New("login error"))
+
+		body, _ := json.Marshal(user)
+		request := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		userHandlers.Login(response, request)
+
+		assert.Equal(t, http.StatusUnauthorized, response.Result().StatusCode)
+	})
+}
+
+func TestUserHandlers_Logout(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cfg := &config.Config{
+		Auth: config.AuthConfig{
+			Jwt: config.JwtConfig{
+				Cookie: config.JwtCookieConfig{
+					Name:     "access_token",
+					MaxAge:   3600,
+					Secure:   true,
+					HttpOnly: true,
+				},
+			},
+		},
+	}
+
+	logger := logger.New(cfg)
+	handlers := NewUserHandlers(cfg, nil, logger)
+
+	t.Run("successful logout", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/logout", bytes.NewBuffer(nil))
+		response := httptest.NewRecorder()
+
+		handlers.Logout(response, request)
+
+		result := response.Result()
+		assert.Equal(t, http.StatusOK, result.StatusCode)
+
+		cookie := response.Result().Cookies()[0]
+		assert.Equal(t, cfg.Auth.Jwt.Cookie.Name, cookie.Name)
+		assert.Equal(t, "", cookie.Value)
+		assert.Equal(t, -1, cookie.MaxAge)
+
+		var responseMsg map[string]string
+		err := json.NewDecoder(response.Body).Decode(&responseMsg)
+		assert.NoError(t, err)
+		assert.Equal(t, "successfully logged out", responseMsg["message"])
+	})
+}
