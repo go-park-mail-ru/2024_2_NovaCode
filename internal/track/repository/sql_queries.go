7b9c1083af623013c688ae9b077d45f91a2b241e
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-30 00:53:30 +0300

    fix sql queries

--- internal/track/repository/sql_queries.go
+++ internal/track/repository/sql_queries.go
@@ -1,16 +1,16 @@
 package repository
 
 const (
-	createTrackQuery = `INSERT INTO track (name, genre, duration, filepath, image, artist_id, album_id, release) 
+	createTrackQuery = `INSERT INTO track (name, genre, duration, filepath, image, artist_id, album_id, release_date) 
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-		RETURNING id, name, genre, duration, filepath, image, artist_id, album_id, release, created_at, updated_at`
+		RETURNING id, name, genre, duration, filepath, image, artist_id, album_id, release_date, created_at, updated_at`
 
 	findByIDQuery = `SELECT id, name, genre, duration, filepath, image, artist_id, album_id,
-  release, created_at, updated_at FROM track WHERE id = $1`
+  release_date, created_at, updated_at FROM track WHERE id = $1`
 
 	getAllQuery = `SELECT id, name, genre, duration, filepath, image, artist_id, album_id, 
-  release, created_at, updated_at FROM track`
+  release_date, created_at, updated_at FROM track`
 
 	findByNameQuery = `SELECT id, name, genre, duration, filepath, image, artist_id, album_id, 
-  release, created_at, updated_at FROM track WHERE name = $1`
+  release_date, created_at, updated_at FROM track WHERE name = $1`
 )
