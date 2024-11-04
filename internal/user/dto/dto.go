baf04ac7ec388418e754729fad099a2e6fdb3350
MatiXxD <ilya_dinesenko@mail.ru>
2024-11-05 02:06:41 +0300

    Add dto for login/register/update

--- internal/user/dto/dto.go
+++ internal/user/dto/dto.go
@@ -5,6 +5,48 @@ import (
 	"github.com/google/uuid"
 )
 
+type RegisterDTO struct {
+	Role     string `json:"role,omitempty"`
+	Username string `json:"username,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func NewUserFromRegisterDTO(registerDTO *RegisterDTO) *models.User {
+	return &models.User{
+		Role:     registerDTO.Role,
+		Username: registerDTO.Username,
+		Email:    registerDTO.Email,
+		Password: registerDTO.Password,
+	}
+}
+
+type LoginDTO struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func NewUserFromLoginDTO(loginDTO *LoginDTO) *models.User {
+	return &models.User{
+		Username: loginDTO.Username,
+		Password: loginDTO.Password,
+	}
+}
+
+type UpdateDTO struct {
+	UserID   uuid.UUID `json:"id,omitempty"`
+	Username string    `json:"username,omitempty"`
+	Email    string    `json:"email,omitempty"`
+}
+
+func NewUserFromUpdateDTO(updateDTO *UpdateDTO) *models.User {
+	return &models.User{
+		UserID:   updateDTO.UserID,
+		Username: updateDTO.Username,
+		Email:    updateDTO.Email,
+	}
+}
+
 type UserDTO struct {
 	ID       uuid.UUID `json:"id"`
 	Email    string    `json:"email,omitempty"`
