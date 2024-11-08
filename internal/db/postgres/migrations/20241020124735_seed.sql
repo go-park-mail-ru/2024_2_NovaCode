d1bae32b0cabc0fb4042e5f61a5d464b4b045de1
damedelion <ivan.shishkov7@yandex.ru>
2024-11-09 01:15:50 +0300

    add data to seed migration

--- internal/db/postgres/migrations/20241020124735_seed.sql
+++ internal/db/postgres/migrations/20241020124735_seed.sql
@@ -10,7 +10,8 @@ VALUES
   ('Country', 'Кантри'),
   ('Hip-Hop', 'Хип-хоп'),
   ('Indie', 'Инди'),
-  ('Alternative', 'Альтернатива');
+  ('Alternative', 'Альтернатива'),
+  ('IDM', 'Электроника');
 
 INSERT INTO artist
     (name, bio, country, image)
@@ -20,7 +21,9 @@ VALUES
     ('Причастие', 'Калужский дуэт, в который входят две вокалистки: Мария Пенская и Ксения Кузина.', 'Russia', 'prichastie.jpeg'),
     ('Брюки бри', 'Группа из Калуги, бывшее название вб.', 'Russia', 'bryuki_bri.jpeg'),
     ('HXVRMXN', 'Pavel Viktorovich Vankovich, better known by his stage name as HXVRMXN, is a Belarusian phonk music producer.', 'Belarus', 'hxvrmxn.jpeg'),
-    ('YONAKA', 'Yonaka (stylised as YONAKA) are an English rock band based in Brighton.', 'England', 'yonaka.jpg');
+    ('YONAKA', 'Yonaka (stylised as YONAKA) are an English rock band based in Brighton.', 'England', 'yonaka.jpg'),
+    ('Yung Lean', 'Yung Lean, is a Swedish rapper. Widely cited as one of the most influential figures in the early cloud rap era, Yung Lean rose to prominence in 2013 with his song "Ginseng Strip 2002", which went viral on YouTube.', 'Sweden', 'yung_lean.jpeg'),
+    ('Thom Yorke', 'Thomas Edward Yorke is an English musician who is the main vocalist and songwriter of the rock band Radiohead. Rolling Stone described Yorke as one of the greatest and most influential singers of his generation. Also known as solo electronics and IDM artist', 'England', 'thom_yorke.jpeg');
 
 INSERT INTO album
     (name, track_count, image, artist_id)
@@ -30,7 +33,9 @@ VALUES
     ('Перламутр', 5, 'prichastie_perlamytr.jpeg', (SELECT id FROM artist WHERE name = 'Причастие')),
     ('Больше никогда', 5, 'bryuki_bri_bolshe_nikogda.jpeg', (SELECT id FROM artist WHERE name = 'Брюки бри')),
     ('Eclipse', 3, 'hxvrmxn_eclipse.jpg', (SELECT id FROM artist WHERE name = 'HXVRMXN')),
-    ('Don''t Wait ''Til Tomorrow', 5, 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA'));
+    ('Don''t Wait ''Til Tomorrow', 5, 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA')),
+    ('Stranger', 5, 'yung_lean_stranger.jpeg', (SELECT id FROM artist WHERE name = 'Yung Lean')),
+    ('The Eraser', 5, 'thom_yorke_the_eraser.png', (SELECT id FROM artist WHERE name = 'Thom Yorke'));
 
 INSERT INTO track
     (name, duration, filepath, image, artist_id, album_id)
@@ -62,7 +67,17 @@ VALUES
     ('Creature', 185, 'yonaka_creature.mp3', 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow')),
     ('Fired Up', 217, 'yonaka_fired_up.mp3', 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow')),
     ('Lose Our Heads', 200, 'yonaka_lose_our_heads.mp3', 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow')),
-    ('Wake Up', 218, 'yonaka_wake_up.mp3', 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow'));
+    ('Wake Up', 218, 'yonaka_wake_up.mp3', 'yonaka_dont_wait_til_tomorrow.jpg', (SELECT id FROM artist WHERE name = 'YONAKA'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow')),
+    ('Agony', 213, 'yung_lean_stranger_1.mp3', 'yung_lean_stranger.jpeg', (SELECT id FROM artist WHERE name = 'Yung Lean'), (SELECT id FROM album WHERE name = 'Stranger')),
+    ('Drop It / Scooter', 245, 'yung_lean_stranger_2.mp3', 'yung_lean_stranger.jpeg', (SELECT id FROM artist WHERE name = 'Yung Lean'), (SELECT id FROM album WHERE name = 'Stranger')),
+    ('Push / Lost Weekend', 173, 'yung_lean_stranger_3.mp3', 'yung_lean_stranger.jpeg', (SELECT id FROM artist WHERE name = 'Yung Lean'), (SELECT id FROM album WHERE name = 'Stranger')),
+    ('Red Bottom Sky', 302, 'yung_lean_stranger_4.mp3', 'yung_lean_stranger.jpeg', (SELECT id FROM artist WHERE name = 'Yung Lean'), (SELECT id FROM album WHERE name = 'Stranger')),
+    ('Muddy Sea', 164, 'yung_lean_stranger_5.mp3', 'yung_lean_stranger.jpeg', (SELECT id FROM artist WHERE name = 'Yung Lean'), (SELECT id FROM album WHERE name = 'Stranger')),
+    ('The Clock', 253, 'thom_yorke_the_eraser_1.mp3', 'thom_yorke_the_eraser.png', (SELECT id FROM artist WHERE name = 'Thom Yorke'), (SELECT id FROM album WHERE name = 'The Eraser')),
+    ('Cymbal Rush', 314, 'thom_yorke_the_eraser_2.mp3', 'thom_yorke_the_eraser.png', (SELECT id FROM artist WHERE name = 'Thom Yorke'), (SELECT id FROM album WHERE name = 'The Eraser')),
+    ('Atoms For Peace', 313, 'thom_yorke_the_eraser_3.mp3', 'thom_yorke_the_eraser.png', (SELECT id FROM artist WHERE name = 'Thom Yorke'), (SELECT id FROM album WHERE name = 'The Eraser')),
+    ('Analyze', 242, 'thom_yorke_the_eraser_4.mp3', 'thom_yorke_the_eraser.png', (SELECT id FROM artist WHERE name = 'Thom Yorke'), (SELECT id FROM album WHERE name = 'The Eraser')),
+    ('The Eraser', 295, 'thom_yorke_the_eraser_5.mp3', 'thom_yorke_the_eraser.png', (SELECT id FROM artist WHERE name = 'Thom Yorke'), (SELECT id FROM album WHERE name = 'The Eraser'));
 
 INSERT INTO genre_artist (genre_id, artist_id) VALUES
   ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM artist WHERE name = 'EKKSTACY')),
@@ -70,7 +85,9 @@ INSERT INTO genre_artist (genre_id, artist_id) VALUES
   ((SELECT id FROM genre WHERE name = 'Pop'), (SELECT id FROM artist WHERE name = 'Причастие')),
   ((SELECT id FROM genre WHERE name = 'Indie'), (SELECT id FROM artist WHERE name = 'Брюки бри')),
   ((SELECT id FROM genre WHERE name = 'Hip-Hop'), (SELECT id FROM artist WHERE name = 'HXVRMXN')),
-  ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM artist WHERE name = 'YONAKA'));
+  ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM artist WHERE name = 'YONAKA')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM artist WHERE name = 'Yung Lean')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM artist WHERE name = 'Thom Yorke'));
 
 INSERT INTO genre_track (genre_id, track_id) VALUES
   ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM track WHERE name = 'i just want to hide my face')),
@@ -100,7 +117,17 @@ INSERT INTO genre_track (genre_id, track_id) VALUES
   ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM track WHERE name = 'Creature')),
   ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM track WHERE name = 'Fired Up')),
   ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM track WHERE name = 'Lose Our Heads')),
-  ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM track WHERE name = 'Wake Up'));
+  ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM track WHERE name = 'Wake Up')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM track WHERE name = 'Agony')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM track WHERE name = 'Drop It / Scooter')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM track WHERE name = 'Push / Lost Weekend')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM track WHERE name = 'Red Bottom Sky')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM track WHERE name = 'Muddy Sea')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM track WHERE name = 'The Clock')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM track WHERE name = 'Cymbal Rush')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM track WHERE name = 'Atoms For Peace')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM track WHERE name = 'Analyze')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM track WHERE name = 'The Eraser'));
 
 INSERT INTO genre_album (genre_id, album_id) VALUES
   ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM album WHERE name = 'misery')),
@@ -108,7 +135,9 @@ INSERT INTO genre_album (genre_id, album_id) VALUES
   ((SELECT id FROM genre WHERE name = 'Pop'), (SELECT id FROM album WHERE name = 'Перламутр')),
   ((SELECT id FROM genre WHERE name = 'Indie'), (SELECT id FROM album WHERE name = 'Больше никогда')),
   ((SELECT id FROM genre WHERE name = 'Hip-Hop'), (SELECT id FROM album WHERE name = 'Eclipse')),
-  ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow'));
+  ((SELECT id FROM genre WHERE name = 'Alternative'), (SELECT id FROM album WHERE name = 'Don''t Wait ''Til Tomorrow')),
+  ((SELECT id FROM genre WHERE name = 'Rap'), (SELECT id FROM album WHERE name = 'Stranger')),
+  ((SELECT id FROM genre WHERE name = 'IDM'), (SELECT id FROM album WHERE name = 'The Eraser'));
 -- +goose StatementEnd
 
 -- +goose Down
