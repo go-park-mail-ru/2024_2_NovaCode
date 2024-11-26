fcf6b3088f5066315e1fdc68f669301f9965520e
Tatiana Bocharova <tanka.bocharova.23@gmail.com>
2024-11-26 22:02:13 +0300

    fixes

--- microservices/album/delivery/grpc/service/handlers.go
+++ microservices/album/delivery/grpc/service/handlers.go
@@ -5,13 +5,12 @@ import (
 
 	"github.com/go-park-mail-ru/2024_2_NovaCode/microservices/album/dto"
 	albumService "github.com/go-park-mail-ru/2024_2_NovaCode/proto/album"
-	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (service *albumsService) FindByID(ctx context.Context, request *albumService.FindByIDRequest) (*albumService.FindByIDResponse, error) {
-	albumID, err := request.GetId()
+	albumID := request.GetId()
 	album, err := service.usecase.View(ctx, albumID)
 	if err != nil {
 		service.logger.Errorf("cannot find album by id: %v", err)
@@ -23,26 +22,10 @@ func (service *albumsService) FindByID(ctx context.Context, request *albumServic
 
 func (service *albumsService) albumDTOToProto(album *dto.AlbumDTO) *albumService.Album {
 	return &albumService.Album{
-		Id:           album.ID,
-		Name:         album.Name,
-		Duration:     album.Duration,
-		FilePath:     album.FilePath,
-		Image:        album.Image,
-		ArtistId:     album.ArtistID,
-		AlbumId:      album.AlbumID,
-		OrderInAlbum: album.OrderInAlbum,
-		ReleaseDate:  album.ReleaseDate,
-	}
-}
-
-func (service *albumsService) albumDTOToProto(album *dto.AlbumDTO) *albumService.Album {
-	return &albumService.Album{
 		Id:          album.ID,
 		Name:        album.Name,
 		ReleaseDate: album.ReleaseDate,
 		Image:       album.Image,
 		ArtistId:    album.ArtistID,
-		CreatedAt:   album.CreatedAt,
-		UpdatedAt:   album.UpdatedAt,
 	}
 }
